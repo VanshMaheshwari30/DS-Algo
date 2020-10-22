615c228c77f4ffb0b68eee45a28f1be7c299e03f
VanshMaheshwari30 <61056118+VanshMaheshwari30@users.noreply.github.com>
2020-10-22 13:59:59 +0530

    Largest Element in Array
    CPP program for finding the index of largest element of array.

---
+++ Array/LargestElement_Basic.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int getLargest(int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        bool flag = true;
+        for(int j=0; j<n; j++)
+        {
+            if (arr[j]>arr[i])
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(flag==true)
+            return i;
+    }
+    return -1;
+}
+
+int main(void)
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array: \n";
+    for (int i=0; i<n; i++)
+        cin >> arr[i];
+    cout << "The index of largest element is: "<< (getLargest(arr,n))+1;
+}
