00107a15af646c08c23049f50dc2bbd4ee44c098
VanshMaheshwari30 <61056118+VanshMaheshwari30@users.noreply.github.com>
2020-11-01 23:41:46 +0530

    Floor of Square Root of number
    CPP program to find floor of square root of number.

---
+++ Array/FloorOfSquareRoot.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int sqRootFloor(int x)
+{
+	int low = 1, high = x, ans = -1;
+    while(low <= high)
+	{
+		int mid = (low + high) / 2;
+        int mSq = mid * mid;
+        if(mSq == x)
+			return mid;
+		else if(mSq > x)
+			high = mid - 1;
+		else
+		{
+			low = mid + 1;
+			ans = mid;
+		}
+	}
+    return ans;
+}
+
+int main(void)
+{
+    int n;
+    cout << "Enter the element whose square root is to be found: ";
+    cin >> n;
+    cout<< "\nThe square root is: " << sqRootFloor(n);
+}
