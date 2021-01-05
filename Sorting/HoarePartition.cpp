415195b4ef224255902a111d3c7b61b10c422bfa
VanshMaheshwari30 <61056118+VanshMaheshwari30@users.noreply.github.com>
2021-01-05 12:04:33 +0530

    Hoare's Partition
    Program to implement Hoare's Partition.

---
+++ Sorting/HoarePartition.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int HPartition(int arr[], int l, int h)
+{   
+    int pivot=arr[l];
+    int i=l-1,j=h+1;
+    while(true){
+        do{
+            i++;
+        }while(arr[i]<pivot);
+        do{
+            j--;
+        }while(arr[j]>pivot);
+        if(i>=j)return j;
+        swap(arr[i],arr[j]);
+    }
+}
+ 
+int main() {
+	
+    int a[10];
+    cout << "Enter 10 elements of array: " << endl;
+    for (int i=0; i<10; i++)
+        cin >> a[i];
+    cout << "Original Array is: ";
+    for (int i=0; i<10; i++)
+        cout << a[i] << " ";
+	int n=sizeof(a)/sizeof(a[0]);
+	cout <<"\n Array after partitioning is: ";
+	HPartition(a,0,n-1);
+	for(int x: a)
+	    cout<<x<<" ";
+}
