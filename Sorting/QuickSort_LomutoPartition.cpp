ab92525d9dfee1ca9bccdafe4f824229fad686cd
VanshMaheshwari30 <61056118+VanshMaheshwari30@users.noreply.github.com>
2021-01-06 13:32:38 +0530

    Quicksort Using Lomuto Partition
    Program to implement quicksort using Lomuto partitioning.

---
+++ Sorting/QuickSort_LomutoPartition.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LPartition(int arr[], int l, int h)
+{   
+    int pivot=arr[h];
+    int i=l-1;
+    for(int j=l;j<=h-1;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[h]);
+    return i+1;
+}
+
+void qSort(int arr[],int l,int h){
+    if(l<h){
+        int p=LPartition(arr,l,h);
+        qSort(arr,l,p-1);
+        qSort(arr,p+1,h);
+    }
+}
+ 
+int main() {
+	
+    int a[10];
+    cout << "Enter 10 elements of array: " << endl;
+    for (int i=0; i<10; i++)
+        cin >> a[i];
+    cout << "Original Array is: ";
+    for (int i=0; i<10; i++)
+        cout << a[i] << " ";
+	int n=sizeof(a)/sizeof(a[0]);
+	cout <<"\n Array after sorting is: ";
+	qSort(a,0,n-1);
+	for(int x: a)
+	    cout<<x<<" ";
+}
